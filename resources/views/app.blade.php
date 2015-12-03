a2854a986cf9eff6cc54dbae73b4a176e0005075
vagrant <vagrant@homestead>
2015-12-03 23:54:14 +0000

    Ajout de la vue planète en THREE js et des marqueurs (les couleurs)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,7 +19,7 @@
 	<![endif]-->
 	@yield('head')
 </head>
-<body>
+<body onload="initialize()">
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -39,6 +39,7 @@
 					<li><a href="{{ url('/socket-example') }}">Socket.io example</a></li>
 					<li><a href="{{ url('/fire') }}" target="_blank">Fire public event</a></li>
 					<li><a href="{{ url('/fire-private') }}" target="_blank">Fire private event</a></li>
+					<li><a href="{{ url('/threejs-planete') }}">Planete</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
@@ -61,6 +62,7 @@
 			</div>
 		</div>
 	</nav>
+	<div id="earth_div"></div>
 
 	@yield('content')
 
