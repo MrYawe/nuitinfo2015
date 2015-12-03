0f69ee1fa5f014ba7c073b49dfa8633cdf99f92f
vagrant <vagrant@homestead>
2015-12-03 00:04:45 +0000

    update socket io url

--- resources/views/socket_example.blade.php
+++ resources/views/socket_example.blade.php
@@ -10,8 +10,7 @@
     <!--<script src="https://cdn.socket.io/socket.io-1.3.5.js"></script>-->
     <script src="{{ asset('assets/js/socket.io/socket.io.js') }}"></script>
     <script>
-        //var socket = io('http://localhost:3000');
-        var socket = io('http://nuitinfo2015.app:3000');
+        var socket = io("{{url()}}:3000");
         
         socket.on("test-channel:App\\Events\\EventName", function(message){
             // increase the power everytime we load test route
