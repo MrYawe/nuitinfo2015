7196679f363d855e33d7486a351c99d0ea2c6d89
Germain <germain_clauss@yahoo.fr>
2015-12-04 01:36:22 +0100

    Pour dylan

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,15 +38,4 @@ Route::get('threejs-example', function () {
     return view('threejs-example');
 });
 
-Route::get('threejs-planete', function () {
-    return view('threejs-planete');
-});
-
-
-Route::get('user-info', function () {
-    return view('user.user-info');
-});
-
-Route::get('vocal', function () {
-    return view('vocal.test');
-});
+Route::resource('report', 'ReportController');
