d73e83c045fec448e70d07d198336c199a336168
vagrant <vagrant@homestead>
2015-12-04 06:35:23 +0000

    add about in help section

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -15,6 +15,8 @@
 		        		<p>To enable the text-to-speech feature, just press the "r" key. Press it again to pause the reading then press it again to resume it. If it is reading too fast, you can restart the reading by a double tap on the "r" key.</p>
 		        	<h3>Font size<span style="display:none;">.</span></h3>
 		        		<p>If the font is too small for you, you can increase its size by clicking on the magnifying glass with the "plus" sign on the top right of the page. You can also decrease the font size by clicking on the magnifying glass with the "minus" sign on the top right of the page.<br>To be faster, you can press the "plus" key to increase the font size and the "minus" key to decrease it.</p>
+		        	<h3>About<span style="display:none;">.</span></h3>
+		        		<p>The home page is made with WEBGL which displays a world map in 3d. Alerts are displayed when tweets related to keywords that we defined are posted (exemple : earthquakes).<br>In the report section, you can send a post wich will allow users to add alerts without tweeting (it's not displayed in the world map yet, maybe in a future version :p).<br>Finally, in the "General advice" section, you can find the correct way to react to every dangerous situation that are displayed in our world map.<br>We also have developped views to display summaries of alerts when you click on a alert of the world map (but this view is not displayed yet but the start is here).</p>	
 		    	</div>
 			</div>
 		</div>
